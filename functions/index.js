1f0366aab400f12a213674743fe5e285f9aa1883
Jess on the Hope <jesshon.py326@gmail.com>
2022-11-07 07:32:42 -0500

    building onMessageReceived function
    will be triggered when the messaging collection is being written to.  the function will retrieve the target user object from the users collection and add a message in messages hashmap.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,24 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const db = admin.firestore
+const usersCollection = db.CollectionGroup("users")
+
+exports.onMessageReceived = functions.firestore.document("messaging/{id")
+    .onCreate(async (snap, context) => {
+        const values = snap.data();
+        const senderEmail = values.senderEmail
+        const targetEmail = values.targetEmail
+        const message = values.message
+        const date = values.date
+
+        // retrieve the target user's object
+        const targetUserSnapshot = await usersCollection.where("userEmail", "==", targetEmail).get()
+        const targerUserDoc = targetUserSnapshot.docs[0]
+        let targetUser = targerUserDoc.data()
+
+        
+        
+    })
+
